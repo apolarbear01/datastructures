edf4518d58aeaf4a41d7631de92c1201fcabee6f
apolarbear01 <109554308+apolarbear01@users.noreply.github.com>
2022-11-09 05:02:27 +0300

    pushed

---
+++ src/MarryingEve.java
@@ -0,0 +1,44 @@
+
+public class MarryingEve {
+
+	
+			public class Node {
+				
+						private Integer number;
+						private Node link;
+						
+						public Node(Integer number, MarryingEve.Node link) {
+							this.number = number;
+							this.link = link;
+						}
+				
+						public Integer getNumber() {
+							return number;
+						}
+				
+						public void setNumber(Integer number) {
+							this.number = number;
+						}
+				
+						public Node getLink() {
+							return link;
+						}
+				
+						public void setLink(Node link) {
+							this.link = link;
+				}
+		
+		
+		private Node head;
+		
+		
+
+		
+		
+		
+		
+		
+	}
+	
+	
+}
